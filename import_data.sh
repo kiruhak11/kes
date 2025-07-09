444a4ad38bf9bfdac656cd463d9e96a565632196
kiruhak-MAC <kiruhak1991@gmail.com>
2025-07-09 18:50:08 +0700

    Импорт

--- import_data.sh
+++ import_data.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# Копируем SQL файл в контейнер
-docker cp import_all.sql kes-site-mysql:/tmp/
+# Очищаем таблицы перед импортом
+docker exec kes-site-mysql /bin/bash -c 'mysql -uroot -prootpassword kes-site -e "DELETE FROM products; DELETE FROM categories;"'
 
-# Импортируем данные
-docker exec kes-site-mysql /bin/bash -c 'mysql -uroot -prootpassword kes-site --default-character-set=utf8 < /tmp/import_all.sql'
+# Копируем файлы с данными в контейнер
+docker cp "categories_rows.sql" kes-site-mysql:/tmp/
+docker cp "products_rows (9).sql" kes-site-mysql:/tmp/products_rows.sql
 
-# Удаляем временный файл
-docker exec kes-site-mysql rm /tmp/import_all.sql
+# Устанавливаем кодировку и импортируем данные
+docker exec kes-site-mysql /bin/bash -c 'mysql -uroot -prootpassword kes-site --default-character-set=utf8 < /tmp/categories_rows.sql'
+docker exec kes-site-mysql /bin/bash -c 'mysql -uroot -prootpassword kes-site --default-character-set=utf8 < /tmp/products_rows.sql'
+
+# Удаляем временные файлы
+docker exec kes-site-mysql rm /tmp/categories_rows.sql /tmp/products_rows.sql
 
 echo "Data import completed successfully!" 
