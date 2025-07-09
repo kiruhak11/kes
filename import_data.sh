7291e2b46919fb3db79a312d4f86f18d6b7e7340
kiruhak-MAC <kiruhak1991@gmail.com>
2025-07-09 18:47:23 +0700

    Обновлен скрипт import_data.sh: изменен способ импорта данных в базу, добавлено удаление временного файла после импорта. Эти изменения упрощают процесс импорта и обеспечивают чистоту временных файлов.

--- import_data.sh
+++ import_data.sh
@@ -4,6 +4,9 @@
 docker cp import_all.sql kes-site-mysql:/tmp/
 
 # Импортируем данные
-docker exec kes-site-mysql mysql -uroot -prootpassword kes-site --default-character-set=utf8 < /tmp/import_all.sql
+docker exec kes-site-mysql /bin/bash -c 'mysql -uroot -prootpassword kes-site --default-character-set=utf8 < /tmp/import_all.sql'
+
+# Удаляем временный файл
+docker exec kes-site-mysql rm /tmp/import_all.sql
 
 echo "Data import completed successfully!" 
