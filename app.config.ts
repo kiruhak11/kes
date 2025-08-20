ddfb5751d60c1e59c50c1c3ea914af883acfc091
kiruhak-MAC <kiruhak1991@gmail.com>
2025-08-20 10:00:37 +0700

    Обновлен код инициализации Yandex.Metrika: перемещен в файл layouts/default.vue для централизованного управления. Упрощено отображение счетчика, добавлены комментарии для ясности. Удалены устаревшие вызовы из app.vue.

--- app.config.ts
+++ app.config.ts
@@ -2,31 +2,26 @@ export default defineAppConfig({
   head: {
     script: [
       {
-        hid: 'yandex-metrika',
+        hid: "yandex-metrika",
         innerHTML: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
         m[i].l=1*new Date();
         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-        ym(103178484, "init", {
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true,
-          webvisor:true
-        });`,
-        type: 'text/javascript',
-        charset: 'utf-8'
-      }
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");`,
+        type: "text/javascript",
+        charset: "utf-8",
+      },
     ],
     noscript: [
       {
-        hid: 'yandex-metrika-noscript',
-        innerHTML: '<div><img src="https://mc.yandex.ru/watch/103178484" style="position:absolute; left:-9999px;" alt="" /></div>'
-      }
+        hid: "yandex-metrika-noscript",
+        innerHTML:
+          '<div><img src="https://mc.yandex.ru/watch/103178484" style="position:absolute; left:-9999px;" alt="" /></div>',
+      },
     ],
     __dangerouslyDisableSanitizersByTagID: {
-      'yandex-metrika': ['innerHTML'],
-      'yandex-metrika-noscript': ['innerHTML']
-    }
-  }
-}); 
+      "yandex-metrika": ["innerHTML"],
+      "yandex-metrika-noscript": ["innerHTML"],
+    },
+  },
+});
--- app.vue
+++ app.vue
@@ -1,15 +1,7 @@
 <template>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-
   <!-- Оборачиваем всё в наш дефолтный лейаут -->
   <NuxtLayout>
-    <div>
-      <img
-        src="https://mc.yandex.ru/watch/103178484"
-        style="position: absolute; left: -9999px"
-        alt=""
-      />
-    </div>
     <FrogModal />
     <NuxtPage />
   </NuxtLayout>
@@ -27,31 +19,6 @@
 </template>
 
 <script setup lang="ts">
-(function (m, e, t, r, i, k, a) {
-  m[i] =
-    m[i] ||
-    function () {
-      (m[i].a = m[i].a || []).push(arguments);
-    };
-  m[i].l = 1 * new Date();
-  for (var j = 0; j < document.scripts.length; j++) {
-    if (document.scripts[j].src === r) {
-      return;
-    }
-  }
-  (k = e.createElement(t)),
-    (a = e.getElementsByTagName(t)[0]),
-    (k.async = 1),
-    (k.src = r),
-    a.parentNode.insertBefore(k, a);
-})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-ym(103178484, "init", {
-  webvisor: true,
-  clickmap: true,
-  accurateTrackBounce: true,
-  trackLinks: true,
-});
 // Подключаем тему, чтобы установить атрибут data-theme при старте
 import { useTheme } from "@/composables/useTheme";
 import { useVisitTracking } from "~/composables/useVisitTracking";
