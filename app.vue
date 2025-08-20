bb0e6143e187692c9705ddd3c6e2f7bc71afd590
kiruhak-MAC <kiruhak1991@gmail.com>
2025-08-20 09:28:51 +0700

    Добавлен код для счетчика Yandex.Metrika в файл app.vue, что позволит отслеживать посещаемость и взаимодействие пользователей с сайтом. Обновлены комментарии для ясности.

--- app.vue
+++ app.vue
@@ -1,5 +1,44 @@
 <template>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript">
+    (function (m, e, t, r, i, k, a) {
+      m[i] =
+        m[i] ||
+        function () {
+          (m[i].a = m[i].a || []).push(arguments);
+        };
+      m[i].l = 1 * new Date();
+      for (var j = 0; j < document.scripts.length; j++) {
+        if (document.scripts[j].src === r) {
+          return;
+        }
+      }
+      (k = e.createElement(t)),
+        (a = e.getElementsByTagName(t)[0]),
+        (k.async = 1),
+        (k.src = r),
+        a.parentNode.insertBefore(k, a);
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(103178484, "init", {
+      webvisor: true,
+      clickmap: true,
+      accurateTrackBounce: true,
+      trackLinks: true,
+    });
+  </script>
+  <noscript
+    ><div>
+      <img
+        src="https://mc.yandex.ru/watch/103178484"
+        style="position: absolute; left: -9999px"
+        alt=""
+      /></div
+  ></noscript>
+  <!-- /Yandex.Metrika counter -->
+
   <!-- Оборачиваем всё в наш дефолтный лейаут -->
   <NuxtLayout>
     <FrogModal />
