f063d718accdc63d2073e91f490bcd286df37f3b
kiruhak-MAC <kiruhak1991@gmail.com>
2025-12-06 13:26:42 +0700

    Обновлён способ инициализации PrismaClient, добавлены параметры конфигурации для улучшения управления подключением к базе данных.

--- server/api/sitemap.xml.get.ts
+++ server/api/sitemap.xml.get.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({});
 
 export default defineEventHandler(async (event) => {
   try {
--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -2,12 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 
+const prismaOptions: any = {};
+
 // Проверяем, что мы не в процессе сборки
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient(prismaOptions);
 } else {
   if (!(global as any).prisma) {
-    (global as any).prisma = new PrismaClient();
+    (global as any).prisma = new PrismaClient(prismaOptions);
   }
   prisma = (global as any).prisma;
 }
